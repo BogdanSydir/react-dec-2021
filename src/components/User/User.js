0776f3089262391cc31d2462aded0714f45781dc
BogdanSydir <taote92@gmail.com>
2022-04-07 14:58:17 +0200

    homework

---
+++ src/components/User/User.js
@@ -0,0 +1,11 @@
+const User = ({user}) => {
+    const {id, name, username} = user;
+    return (
+        <div>
+            asdasd
+            {/*{id} -- {name} -- {username}*/}
+        </div>
+    );
+};
+
+export {User};
